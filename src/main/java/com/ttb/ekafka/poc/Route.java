7b9614002beada94c11e89b50a08c83037154413
Audomsak Suksunti <s.audomsak@gmail.com>
2025-04-10 22:31:26 +0700

    handle exception in the Route

--- src/main/java/com/ttb/ekafka/poc/Route.java
+++ src/main/java/com/ttb/ekafka/poc/Route.java
@@ -2,6 +2,7 @@ package com.ttb.ekafka.poc;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 
 @ApplicationScoped
@@ -11,6 +12,12 @@ public class Route extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        onException(Exception.class)
+                .handled(true)
+                .logHandled(true)
+                .logStackTrace(true)
+                .log(LoggingLevel.ERROR, "There was an error occurred. ${exception}");
+
         from("timer://counter?fixedRate=true&period={{timer.interval}}")
                 .routeId("data-generator")
                 .process(counter)
